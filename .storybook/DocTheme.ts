f7aa33fc1df00e8ef1ad0f86c789b1de23454799
robsongdev <robsong369@gmail.com>
2024-02-29 12:01:26 -0300

    add thema pessoal

---
+++ .storybook/DocTheme.ts
@@ -0,0 +1,10 @@
+import {create} from '@storybook/theming'
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Nome da empresa',
+    brandUrl: '',
+    brandImage: '',
+    brandTarget: '_self'
+
+})
