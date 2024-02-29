d5d1eddc2b5bd91144c7b7e7266501649585ec47
robsongdev <robsong369@gmail.com>
2024-02-29 14:19:49 -0300

    add font

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as S from "./styles";
 
 export interface ButtonProps {
+    /**Teste comentario de props*/
     variant?: "primary" | "accent";
     disabled?: boolean;
     children: React.ReactNode;
